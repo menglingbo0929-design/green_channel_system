fea0fa38c23deb92e468251ee254f5d3c87f9d5c
CCB <ccb776689@163.com>
2026-07-17 21:51:54 +0800

    完善数据库

--- backend/src/main/java/com/example/backend/web/controller/UserController.java
+++ backend/src/main/java/com/example/backend/web/controller/UserController.java
@@ -1,12 +1,27 @@
 package com.example.backend.web.controller;
 
+import com.example.backend.common.JsonResponse;
+import com.example.backend.model.domain.User;
+import com.example.backend.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
+
 public class UserController {
-
-
+@Autowired
+private IUserService userService;
+    @PostMapping("login")
+    public JsonResponse login(@RequestBody User user){
+        User login=userService.login(user);
+        if (login == null) {
+            return JsonResponse.failure("用户名或密码错误");
+        }
+        return JsonResponse.success(login);
+    }
 
 }
 
